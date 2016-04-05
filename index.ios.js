7bd42233e77170ebfe2130e5a5edbf307041613c
fchasen <fchasen@gmail.com>
2016-04-05 14:11:56 -0400

    added files

---
+++ index.ios.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const Reader = require('./app/Reader');
+
+class EpubReader extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Reader style={styles.reader} src={"https://s3.amazonaws.com/moby-dick/OPS/package.opf"}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  reader: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+});
+
+AppRegistry.registerComponent('EpubReader', () => EpubReader);
