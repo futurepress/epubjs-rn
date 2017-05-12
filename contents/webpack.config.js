452ac101722d9063f068d325b6e5f084489e7f57
fchasen <fchasen@gmail.com>
2017-05-12 13:54:25 -0400

    Fixed Contents for Android

--- contents/webpack.config.js
+++ contents/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 		loaders: [
 			{
 				test: /\.js$/,
-				exclude: /node_modules/,
+				//exclude: /node_modules/,
 				loader: "babel-loader",
 				query: {
 					presets: ['es2015']
