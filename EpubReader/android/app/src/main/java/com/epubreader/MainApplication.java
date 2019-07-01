c629f98c04c35add783edcc3d86264f4994a0934
Fred Chasen <fchasen@gmail.com>
2019-06-30 22:44:24 -0700

    Updated dependencies

--- EpubReader/android/app/src/main/java/com/epubreader/MainApplication.java
+++ EpubReader/android/app/src/main/java/com/epubreader/MainApplication.java
@@ -53,5 +53,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    WebView.setWebContentsDebuggingEnabled(true);
   }
 }
